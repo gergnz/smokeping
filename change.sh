92152f19e0f00276233d00b5d1d035c49cafaa8d
Greg Cockburn <gergnz@gmail.com>
2022-03-26 17:33:35 +1100

    begin doing the dark change

---
+++ change.sh
@@ -0,0 +1,7 @@
+
+
+/usr/share/perl5/vendor_perl/Smokeping.pm
+           '--imgformat','PNG',
+           '--color', 'FONT#32a852FF',
+           '--color', 'BACK#232323FF',
+           '--color', 'CANVAS#7c7c7cFF',
